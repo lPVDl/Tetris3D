1ad104b14cd611d73fa9650aab7107802d5756ca
Victor Fuchadzhy <pvd1313@gmail.com>
2020-07-20 18:26:29 +0300

    Move level grid to separate installer.

---
+++ Assets/Scripts/Game/Core/LevelGrid/Installers/CoreLevelGridInstaller.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using Zenject;
+
+namespace Game.Core.LevelGrid
+{
+    [CreateAssetMenu(menuName = "ScriptableObject/Installers/Game/Core/LevelGrid", fileName = "CoreLevelGridInstaller")]
+    public class CoreLevelGridInstaller : ScriptableObjectInstaller
+    {
+        [SerializeField] private LevelGridView _gridView;
+
+        public override void InstallBindings()
+        {
+            Container.Bind<LevelGridView>().FromComponentInNewPrefab(_gridView).AsSingle().NonLazy();
+        }
+    }
+}
---
+++ Assets/Scripts/Game/Core/LevelGrid/Views/LevelGridView.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+
+namespace Game.Core.LevelGrid
+{
+    public class LevelGridView : MonoBehaviour
+    {
+
+    }
+}
