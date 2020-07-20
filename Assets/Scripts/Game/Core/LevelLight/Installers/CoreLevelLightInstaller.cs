3bd917bd3c155d92b30d5a7bc07a3cf42a52a867
Victor Fuchadzhy <pvd1313@gmail.com>
2020-07-20 18:34:01 +0300

    Move light to separate installer

---
+++ Assets/Scripts/Game/Core/LevelLight/Installers/CoreLevelLightInstaller.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using Zenject;
+
+namespace Game.Core.LevelLight
+{
+    [CreateAssetMenu(menuName = "ScriptableObject/Installers/Game/Core/LevelLight", fileName = "CoreLevelLightInstaller")]
+    public class CoreLevelLightInstaller : ScriptableObjectInstaller
+    {
+        [SerializeField] private LevelLightView _levelLightView;
+
+        public override void InstallBindings()
+        {
+            Container.Bind<LevelLightView>().FromComponentInNewPrefab(_levelLightView).AsSingle().NonLazy();            
+        }
+    }
+}
---
+++ Assets/Scripts/Game/Core/LevelLight/Views/LevelLightView.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+
+namespace Game.Core.LevelLight
+{
+    public class LevelLightView : MonoBehaviour
+    {
+
+    }
+}
